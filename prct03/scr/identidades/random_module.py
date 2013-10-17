7665de555823c69f3d7d8b4dcedebb62a0db4698
Alexandra <alelit4@gmail.com>
2013-10-17 15:27:06 -0400

    some colors

--- prct03/scr/identidades/random_module.py
+++ prct03/scr/identidades/random_module.py
@@ -7,17 +7,34 @@ import random
 # print "Number of arguments: ", len(sys.argv)
 # print "The arguments are: " , str(sys.argv)
 
-#A = float(sys.argv[1])
-#B = float(sys.argv[2])
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+
+    def disable(self):
+        self.HEADER = ''
+        self.OKBLUE = ''
+        self.OKGREEN = ''
+        self.WARNING = ''
+        self.FAIL = ''
+        self.ENDC = ''
+
 A = -100
 B = 100
+
+
 
 if  len(sys.argv) == 2:
     for i in range(0,int(sys.argv[1])):
         a = random.uniform(A, B)
         b = random.uniform(A, B)
-        print '--------- i = ', i+1, '---------'
-        print 'a = ', a
-        print 'b = ', b 
+        print bcolors.HEADER + '--------- i = ', i+1, '---------'
+       
+        print  bcolors.OKBLUE + 'a = ', a
+        print bcolors.OKGREEN + 'b = ', b, bcolors.ENDC
 else:
-    print 'Debe introducir un dato en argv'
+    print bcolors.FAIL + "FAIL: No arguments. Try again!" + bcolors.ENDC
